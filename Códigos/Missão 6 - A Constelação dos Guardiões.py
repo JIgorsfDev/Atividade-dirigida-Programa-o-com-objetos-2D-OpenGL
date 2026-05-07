a1ec8c1979819887b941e4985d64b6401fb8e43e
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-05-07 20:45:15 -0300

    Add star constellation drawing functionality
    Implement function to draw a constellation of stars with interactivity.

--- Códigos/Missão 6 - A Constelação dos Guardiões.py
+++ Códigos/Missão 6 - A Constelação dos Guardiões.py
@@ -1,1 +1,9 @@
-
+#Construa a “Constelação dos Guardiões”, formada por um conjunto de círculos de diferentes tamanhos, que representam estrelas mágicas conectadas por propósito. Crie uma função desenharEstrela(x, y, raio) para
+#desenhar um círculo em uma posição específica. Utilize essa função para montar uma constelação composta por pelo menos 7 estrelas, cada uma com:
+#1- Um raio diferente (variação entre 10 e 50) e cores aleatórias gerada a cada execução.
+#2- Conexões entre estrelas feitas por linhas finas (use GL_LINES).
+#3- Recursos interativos:
+#4- Pressionar a tecla "n" para adicionar uma nova estrela com raio e posição aleatórios.
+#5- Pressionar a tecla "x" para remover uma das estrelas (pode ser aleatoriamente ou não).
+#6- Pressionar a tecla "r" reinicia a constelação.
+#7- Pressionar "t" ativa o “modo noturno” (plano de fundo preto e estrelas brilhantes em branco ou amarelo).
