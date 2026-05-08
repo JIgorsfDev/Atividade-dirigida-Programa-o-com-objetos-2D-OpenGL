cc0c3747fdd5b7ae67b69759204a8066d2b1af28
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-05-08 13:02:59 -0300

    Implement drawing of multiple isosceles triangles
    Added functionality to draw multiple isosceles triangles with user-defined dimensions and random positions. Improved the draw function to utilize a list of triangles.

--- Códigos/Missão 3 - A aliança dos triângulos.py
+++ Códigos/Missão 3 - A aliança dos triângulos.py
@@ -1,6 +1,5 @@
 #Desenvolva uma função que desenhe triângulos isósceles com base e altura fornecidas pelo usuário. Em seguida, use essa função para desenhar pelo menos 5 triângulos com diferentes tamanhos e posições em tela. 
 #Cada triângulo deve ter uma cor distinta.
-#incompleto
 
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
@@ -11,59 +10,50 @@ r = 0
 g = 0
 b = 0
 
+triangulos = []
+
+for i in range(5):
+    bs = float(input("Digite a base do triângulo (entre -1 e 1): "))
+    h = float(input("Digite a altura do triângulo (entre -1 e 1): "))
+
+    x = rd.uniform(-1, 1 - bs)
+    y = rd.uniform(-1, 1 - h)
+
+    triangulos.append((bs, h, x, y))
+
+
 def teclado(tecla, x, y):
-  if tecla == b'\x1b':
-    sys.exit()
+    if tecla == b'\x1b':
+        sys.exit()
+
 
 def cor():
-  global r, g, b
-  r = rd.random()
-  g = rd.random()
-  b = rd.random()
+    global r, g, b
+    r = rd.random()
+    g = rd.random()
+    b = rd.random()
 
-def draw_triangles(x, y, bs, h):
+
+def draw_triangles(bs, h, x, y):
     gl.glBegin(gl.GL_TRIANGLES)
     cor()
     gl.glColor3f(r, g, b)
     gl.glVertex2f(x, y)
     gl.glVertex2f(x + bs, y)
-    gl.glVertex2f(x + bs/2, y + h)
+    gl.glVertex2f(x + bs / 2, y + h)
     gl.glEnd()
-   
-  
-
-  
-
-bs = float(input("Digite a base do triangulo (entre -1 e 1): "))
-h = float(input("Digite a altura do triangulo (entre -1 e 1): "))
 
 
 def draw():
-  gl.glClearColor(1.0, 1.0, 1.0, 1.0)
-  gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-  
-  cor()
-  global bs, h
-  x = -bs/2
-  y = -h/2
-  
-  draw_triangles(x, y, bs, h)
-  x += 0.1 #trocar isso, é pros triangulos mudarem de posição e tamanho 
-  y += 0.1
-  draw_triangles(x, y, bs, h)
-  x += 0.1
-  y += 0.1
-  draw_triangles(x, y, bs, h)
-  x += 0.1
-  y += 0.1
-  draw_triangles(x, y, bs, h)
-  x += 0.1
-  y += 0.1
-  draw_triangles(x, y, bs, h)
-  x += 0.1
-  y += 0.1
+    gl.glClearColor(1.0, 1.0, 1.0, 1.0)
+    gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
-  gl.glFlush()
+    for i in triangulos:
+        bs, h, x, y = i
+        draw_triangles(bs, h, x, y)
+
+    gl.glFlush()
+
 
 glut.glutInit()
 glut.glutInitDisplayMode(0)
@@ -72,4 +62,3 @@ glut.glutReshapeWindow(500, 500)
 glut.glutDisplayFunc(draw)
 glut.glutKeyboardFunc(teclado)
 glut.glutMainLoop()
-
